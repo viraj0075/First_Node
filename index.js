33007163c5cc4f39885d5e42ad13579df0bcbdc7
viraj0075 <viraj.koradia.learn.7979@gmail.com>
2024-04-21 18:28:59 +0530

    Entering the json data

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT;
 
+const jsondata  = {
+    "browsers": {
+      "firefox": {
+        "name": "Firefox",
+        "pref_url": "about:config",
+        "releases": {
+          "1": {
+            "release_date": "2004-11-09",
+            "status": "retired",
+            "engine": "Gecko",
+            "engine_version": "1.7"
+          }
+        }
+      }
+    }
+  }
+  
+
 
 app.get("/",(req,res) =>{
     res.send("Hellon Viraj");
@@ -16,6 +34,10 @@ app.get("/chaiaurcode",(req,res) =>{
     res.send("<h1>This is Chai aur code </h1>");
 })
 
+
+app.get("/jsondata",(req,res) =>{
+    res.json(jsondata);
+})
 
 app.listen(port,()=>{
     console.log(`Your Server is listening on port ${port}`);
