410da1d2e04af63e3900cf15437736c74f448ec1
viraj0075 <viraj.koradia.learn.7979@gmail.com>
2024-05-12 19:20:48 +0530

    Fixes #3 : Learned to created the Models

---
+++ Lecture-3/models/todos.models.js
@@ -0,0 +1,25 @@
+import mongoose  from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    complete:{
+        type:Boolean,
+        default:false
+    },
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    subTodos:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"SubTodo"
+        }
+    ]
+
+});
+
+export const todo =  mongoose.model("Todo",todoSchema);
