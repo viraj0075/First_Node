694cd6c52eb4ebf36e126f2b418dde41a833cd95
viraj0075 <viraj.koradia.learn.7979@gmail.com>
2024-05-18 10:10:24 +0530

    Fixes #9 :Done

--- Lecture-4/src/app.js
+++ Lecture-4/src/app.js
@@ -3,10 +3,18 @@ import cors from "cors";
 
 
 const app =  express();
+
+//only Allowed origin
 app.use(cors(
     {
         origin:process.env.CORS_ORIGIN
     }
 ))
-
+//for the limit the Size of the JSON
+app.use(express.json({limit:"16kb"}))
+//for the url encoded to the spedfic object 
+// here we can give th nested object.
+app.use(express.urlencoded({extended:true}));
+//for store the pdf file in the server
+app.use(express.static("public"))
 export default app;
